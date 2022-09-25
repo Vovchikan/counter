acb5e5fa3b4bb345c4a917331ef2848f9eebe8bf
Vladimir Samorodov <vovchikan@gmail.com>
2022-09-25 19:16:39 +0500

    Add test for delay 60 sec

---
+++ test/counter_get_value_SUITE.erl
@@ -0,0 +1,20 @@
+-module (counter_get_value_SUITE).
+
+-include_lib ("common_test/include/ct.hrl").
+
+-export ([all/0]).
+
+-export ([simple_get_value_test/1]).
+ 
+all() -> [simple_get_value_test].
+
+-define (N, 100).
+-define (SPAWNS, 10).
+
+simple_get_value_test (_Config) ->
+  ok = application:start (counter),
+  lists:foreach (fun (_) -> counter_srv:incr() end, lists:seq(1, ?N)),
+  ?N = counter_srv:get_value(),
+  timer:sleep (60 * 1000),
+  0 = counter_srv:get_value(),
+  application:stop (counter).
